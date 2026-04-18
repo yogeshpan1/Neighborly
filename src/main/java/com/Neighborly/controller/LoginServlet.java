b96644e5962faac969e076c1a45137a41ed5a763
Rikesh Adhikari <rikeshadhik@gmail.com>
2026-04-18 21:11:08 +0545

    Added Login Function

--- src/main/java/com/Neighborly/controller/LoginServlet.java
+++ src/main/java/com/Neighborly/controller/LoginServlet.java
@@ -14,29 +14,19 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.getRequestDispatcher("/WEB-INF/Pages/login.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        String users = request.getParameter("username");
+        
+        request.setAttribute("username", users);
+        
+        request.getRequestDispatcher("/WEB-INF/Pages/home.jsp").forward(request, response);
+    }
 }
