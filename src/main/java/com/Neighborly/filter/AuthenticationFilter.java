ddf7f0535fcaff17d4edf74a2b97993458a2dd4b
Rikesh Adhikari <rikeshadhik@gmail.com>
2026-04-20 14:15:19 +0545

    Revert "Added LogoutServlet"
    This reverts commit 3e30c6a07f6c3490b99faa9d2cc1035620878344.

--- src/main/java/com/Neighborly/filter/AuthenticationFilter.java
+++ src/main/java/com/Neighborly/filter/AuthenticationFilter.java
@@ -7,51 +7,35 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-import com.Neighborly.utils.SessionUtil;
-
-/**
- * Servlet Filter implementation class AuthenticationFilter
- */
-
 @WebFilter(urlPatterns = {"/home","/feed","/news","/logout"})
-public class AuthenticationFilter extends HttpFilter implements Filter {
-       
-    private static final long serialVersionUID = 1L;
+public class AuthenticationFilter implements Filter {
 
     public AuthenticationFilter() {
-        super();
     }
 
-	public void destroy() {
-	}
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        HttpSession session = req.getSession(false);
+        
+        // If the "user" session exists, allow the request; otherwise, redirect to /login
+        if (session != null && session.getAttribute("user") != null) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
 
-		boolean isLoggedIn = SessionUtil.getAttribute(httpRequest, "user") != null;
-
-		if (isLoggedIn) {
-			chain.doFilter(request, response);
-		} else {
-
-			httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-			httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
-		}
-	}
-
-	public void init(FilterConfig fConfig) throws ServletException {
-	}
-
+    public void destroy() {
+    }
 }
--- src/main/java/com/Neighborly/filter/GuestFilter.java
+++ src/main/java/com/Neighborly/filter/GuestFilter.java
@@ -2,36 +2,39 @@ package com.Neighborly.filter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-import com.Neighborly.utils.SessionUtil;
-
 @WebFilter(urlPatterns = {"/login", "/register"})
-public class GuestFilter extends HttpFilter {
-    
-    private static final long serialVersionUID = 1L;
+public class GuestFilter implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-            throws IOException, ServletException {
+    public GuestFilter() {
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
         
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-		boolean isLoggedIn = SessionUtil.getAttribute(httpRequest, "user") != null;
-
-        if (isLoggedIn) {
-            httpResponse.sendRedirect(httpRequest.getContextPath() + "/dashboard");
+        HttpSession session = req.getSession(false);
+        
+        if (session != null && session.getAttribute("user") != null) {
+            res.sendRedirect(req.getContextPath() + "/home");
         } else {
             chain.doFilter(request, response);
         }
+    }
+
+    public void destroy() {
     }
 }
