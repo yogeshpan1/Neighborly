3e3b36e5bf4406d55e28ecf0c6de0339b80622c2
Arjan204 <arjanregmi2006@gmail.com>
2026-05-01 22:42:26 +0545

    Adding the Creation code for the Poll Dao

--- src/main/java/com/Neighborly/dao/PollDAO.java
+++ src/main/java/com/Neighborly/dao/PollDAO.java
@@ -1,7 +1,32 @@
 package com.Neighborly.dao;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Neighborly.utils.DBconfig;
 
 public class PollDAO 
 {
 
-    
+	 public void insertPoll(String question, String status, int userId) throws Exception 
+	 {
+	        Connection con = DBconfig.getConnection();
+	        
+	        String sql = "INSERT INTO Polls (Poll_Question, Poll_Status, User_ID) VALUES (?, ?, ?)";
+	        
+	        PreparedStatement pst = con.prepareStatement(sql);
+	        
+	        pst.setString(1, question);
+	        pst.setString(2, status);
+	        pst.setInt(3, userId);
+	        pst.executeUpdate();
+	        pst.close();
+	        con.close();
+	    }
+	 
+	
 }
