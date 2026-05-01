35c645a4c295abf32f2c78b900d0fd7bffdfffe9
Arjan204 <arjanregmi2006@gmail.com>
2026-05-01 22:27:20 +0545

    adding the getter methods and attributes

--- src/main/java/com/Neighborly/model/PollModel.java
+++ src/main/java/com/Neighborly/model/PollModel.java
@@ -1,5 +1,38 @@
 package com.Neighborly.model;
 
-public class PollModel {
+public class PollModel 
+{
+	 private int pollId;
+	 private String question;
+	 private String status;
+	 private String createdAt;
+	 private int userId;
+	 
+	// Getters
+	public int getPollId() 
+	{ 
+	    return pollId; 
+	}
+
+	public String getQuestion() 
+	{ 
+	    return question; 
+	}
+
+	public String getStatus() 
+	{ 
+	    return status; 
+	}
+
+	public String getCreatedAt() 
+	{ 
+	    return createdAt; 
+	}
+
+	public int getUserId() 
+	{ 
+	    return userId;
+	}
+
 
 }
