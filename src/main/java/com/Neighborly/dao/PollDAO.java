f27a5bc08ce166c350ab1bebfe74133bdafe16c0
Arjan204 <arjanregmi2006@gmail.com>
2026-05-01 22:57:29 +0545

    Adding the poll reading code but requries a servlet

--- src/main/java/com/Neighborly/dao/PollDAO.java
+++ src/main/java/com/Neighborly/dao/PollDAO.java
@@ -28,5 +28,36 @@ public class PollDAO
 	        con.close();
 	    }
 	 
+	   public List<Poll> getAllPolls() throws Exception
+	   {
+	        List<Poll> polls = new ArrayList<>();
+	        
+	        Connection con = DBconfig.getConnection();
+	        
+	        String sql = "SELECT * FROM Polls";
+	        
+	        PreparedStatement pst = con.prepareStatement(sql);
+	        
+	        ResultSet rs = pst.executeQuery();
+	        
+	        while (rs.next()) 
+	        {
+	            Poll p = new Poll();
+	            
+	            p.setPollId(rs.getInt("Poll_ID"));
+	            p.setQuestion(rs.getString("Poll_Question"));
+	            p.setStatus(rs.getString("Poll_Status"));
+	            p.setCreatedAt(rs.getString("Poll_CreatedAt"));
+	            p.setUserId(rs.getInt("User_ID"));
+	            polls.add(p);
+	        }
+	        
+	        rs.close();
+	        pst.close();
+	        con.close();
+	        return polls;
+	    }
+	 
+	 
 	
 }
