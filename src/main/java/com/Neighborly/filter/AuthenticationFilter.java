6c775f5ecefdfed8f20831cd3a221abb22b93af4
Rikesh Adhikari <rikeshadhik@gmail.com>
2026-05-02 11:58:58 +0545

    Issue Fix

--- src/main/java/com/Neighborly/filter/AuthenticationFilter.java
+++ src/main/java/com/Neighborly/filter/AuthenticationFilter.java
@@ -19,7 +19,7 @@ import com.Neighborly.utils.SessionUtil;
  * Servlet Filter implementation class AuthenticationFilter
  */
 
-@WebFilter(urlPatterns = {"/home","/feed","/news","/logout"})
+@WebFilter(urlPatterns = {"/home","/feed","/news","/profile","/logout"})
 public class AuthenticationFilter extends HttpFilter implements Filter {
        
     private static final long serialVersionUID = 1L;
