3b46bdc5b6f91647ca68529759a312e0c9f51092
Arjan204 <arjanregmi2006@gmail.com>
2026-05-03 23:17:35 +0545

    Chnaging the poll dao query

--- src/main/java/com/Neighborly/dao/PollDAO.java
+++ src/main/java/com/Neighborly/dao/PollDAO.java
@@ -14,7 +14,7 @@ public class PollDAO {
 	public void insertPoll(String question, String description, String option1, String option2, String status,
 			int adminId) throws Exception {
 		Connection con = DBconfig.getConnection();
-		String sql = "INSERT INTO polls (Poll_Question, Poll_Description, Poll_Option1, Poll_Option2, Poll_Status, Admin_ID) VALUES (?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO polls (poll_question, poll_description, poll_option1, poll_option2, poll_status, admin_id) VALUES (?, ?, ?, ?, ?, ?)";
 		PreparedStatement pst = con.prepareStatement(sql);
 		pst.setString(1, question);
 		pst.setString(2, description);
@@ -32,7 +32,7 @@ public class PollDAO {
 
 		Connection con = DBconfig.getConnection();
 
-		String sql = "SELECT Poll_ID, Poll_Question, Poll_Description, Poll_Option1, Poll_Option2, Poll_Status, Poll_CreatedAt, Admin_ID FROM polls WHERE Poll_Status = 'Active'";
+		String sql = "SELECT poll_id, poll_question, poll_description, poll_option1, poll_option2, poll_status, poll_created_at, admin_id FROM polls WHERE poll_status = 'Active'";
 
 		PreparedStatement pst = con.prepareStatement(sql);
 
@@ -41,14 +41,14 @@ public class PollDAO {
 		while (rs.next()) {
 			PollModel p = new PollModel();
 
-			p.setPollId(rs.getInt("Poll_ID"));
-			p.setQuestion(rs.getString("Poll_Question"));
-			p.setDescription(rs.getString("Poll_Description"));
-			p.setOption1(rs.getString("Poll_Option1"));
-			p.setOption2(rs.getString("Poll_Option2"));
-			p.setStatus(rs.getString("Poll_Status"));
-			p.setCreatedAt(rs.getString("Poll_CreatedAt"));
-			p.setAdminId(rs.getInt("Admin_ID"));
+			p.setPollId(rs.getInt("poll_id"));
+			p.setQuestion(rs.getString("poll_question"));
+			p.setDescription(rs.getString("poll_description"));
+			p.setOption1(rs.getString("poll_option1"));
+			p.setOption2(rs.getString("poll_option2"));
+			p.setStatus(rs.getString("poll_status"));
+			p.setCreatedAt(rs.getString("poll_created_at"));
+			p.setAdminId(rs.getInt("admin_id"));
 			polls.add(p);
 		}
 
@@ -61,7 +61,7 @@ public class PollDAO {
 	public void updatePoll(int pollId, String question, String description, String option1, String option2)
 			throws Exception {
 		Connection con = DBconfig.getConnection();
-		String sql = "UPDATE polls SET Poll_Question = ?, Poll_Description = ?, Poll_Option1 = ?, Poll_Option2 = ? WHERE Poll_ID = ?";
+		String sql = "UPDATE polls SET poll_question = ?, poll_description = ?, poll_option1 = ?, poll_option2 = ? WHERE poll_id = ?";
 		PreparedStatement pst = con.prepareStatement(sql);
 		pst.setString(1, question);
 		pst.setString(2, description);
@@ -74,7 +74,7 @@ public class PollDAO {
 	}
 
 	public boolean deletePoll(int pollId) throws Exception {
-		String sql = "UPDATE polls SET Poll_Status = 'Inactive' WHERE Poll_ID = ?";
+		String sql = "UPDATE polls SET poll_status = 'Inactive' WHERE poll_id = ?";
 
 		Connection con = DBconfig.getConnection();
 
